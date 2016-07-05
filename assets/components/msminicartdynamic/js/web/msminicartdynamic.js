135f24baabedb7a5aec71b92a7427d28a6654876
marabar <marabar@marabar-MB50IA1>
2016-07-05 22:36:30 +0500

    Обновление мини корзины после стандартного добавления товара (без +/-)

--- assets/components/msminicartdynamic/js/web/msminicartdynamic.js
+++ assets/components/msminicartdynamic/js/web/msminicartdynamic.js
@@ -56,69 +56,83 @@ var msMiniCartDynamic = {
 
 $(document).ready(function(){
 
-    $( msMiniCartDynamic.init.selectorAction ).bind( 'keypress', function(b){
-            var C = /[0-9\x25\x24\x23\x13]/;
-            var a = b.which;
-            var c = String.fromCharCode(a);
+	$( msMiniCartDynamic.init.selectorAction ).bind( 'keypress', function(b){
+		var C = /[0-9\x25\x24\x23\x13]/;
+		var a = b.which;
+		var c = String.fromCharCode(a);
 
-            return !!(a==0||a==8||a==9||a==13||c.match(C));
-    });
+		return !!(a==0||a==8||a==9||a==13||c.match(C));
+	});
 
-    $( document ).on( 'change keypress', msMiniCartDynamic.init.selectorAction, function( event ){
-            var selectorForm = '#' +  $( this ).closest( 'form' ).attr( 'id' );
-            var count = parseInt($( this ).val());
-            var c = $( selectorForm + ' .dynamic-action' ).val();
-            
-            if ( event.which == 13 ) {
-                    
-                    if ( c == 'cart/add' ) {
-                            msMiniCartDynamic.toCartDynamic( selectorForm );
-                    }
-                    else {
-                            msMiniCartDynamic.changeCartDynamic( selectorForm, count );
-                    }
-                    $(this).blur();
-                    
-                    return false;
-            }
-            else if ( event.type == 'change' ) {
-                    $( this ).closest( 'form' ).submit();
-                    
-                    if ( c == 'cart/add' ) {
-                            msMiniCartDynamic.toCartDynamic( selectorForm );
-                    }
-                    else {
-                            msMiniCartDynamic.changeCartDynamic( selectorForm, count );
-                    }
-            }
-    });
-    
-    $( document ).on( 'click', msMiniCartDynamic.init.selectorClick, function(e) {
-            e.preventDefault();
-            var selectorForm = '#' +  $( this ).closest( 'form' ).attr( 'id' );
-            var current = $( selectorForm + ' input[name=count]');
-            var count = parseInt(current.val());
-            var dynamic = parseInt( $( this ).data( 'dynamic' ) );
-            var key = $( selectorForm + ' .dynamic-key' );
-            
-            if ( isNaN( count ) && dynamic == +1 ) {
-                    count = 0;
-                    current.val( count + dynamic ).submit();
-                    
-                    msMiniCartDynamic.toCartDynamic( selectorForm );
-            }
-            else if ( isNaN( count ) && dynamic == -1 ) {
-                    return false;
-            }
-            else if ( count == 1 && dynamic == -1 ) {
-                    current.val( '' ).submit();
-                    msMiniCartDynamic.changeCartDynamic( selectorForm, count - 1 );
-                    $( selectorForm + ' .dynamic-action' ).val( 'cart/add' );
-            }
-            else {
-                    current.val( count + dynamic ).submit();
-                    msMiniCartDynamic.changeCartDynamic( selectorForm, count );
-                    $( selectorForm + ' .dynamic-action' ).val( 'cart/change' );
-            }
-    });
+	$( document ).on( 'change keypress', msMiniCartDynamic.init.selectorAction, function( event ){
+		var selectorForm = '#' +  $( this ).closest( 'form' ).attr( 'id' );
+		var count = parseInt($( this ).val());
+		var c = $( selectorForm + ' .dynamic-action' ).val();
+
+		if ( event.which == 13 ) {
+
+			if ( c == 'cart/add' ) {
+				msMiniCartDynamic.toCartDynamic( selectorForm );
+			}
+			else {
+				msMiniCartDynamic.changeCartDynamic( selectorForm, count );
+			}
+			$(this).blur();
+
+			return false;
+		}
+		else if ( event.type == 'change' ) {
+			$( this ).closest( 'form' ).submit();
+
+			if ( c == 'cart/add' ) {
+				msMiniCartDynamic.toCartDynamic( selectorForm );
+			}
+			else {
+				msMiniCartDynamic.changeCartDynamic( selectorForm, count );
+			}
+		}
+	});
+
+	$( document ).on( 'click', msMiniCartDynamic.init.selectorClick, function(e) {
+		e.preventDefault();
+		var selectorForm = '#' +  $( this ).closest( 'form' ).attr( 'id' );
+		var current = $( selectorForm + ' input[name=count]');
+		var count = parseInt(current.val());
+		var dynamic = parseInt( $( this ).data( 'dynamic' ) );
+		var key = $( selectorForm + ' .dynamic-key' );
+
+		if ( isNaN( count ) && dynamic == +1 ) {
+			count = 0;
+			current.val( count + dynamic ).submit();
+
+			msMiniCartDynamic.toCartDynamic( selectorForm );
+		}
+		else if ( isNaN( count ) && dynamic == -1 ) {
+			return false;
+		}
+		else if ( count == 1 && dynamic == -1 ) {
+			current.val( '' ).submit();
+			msMiniCartDynamic.changeCartDynamic( selectorForm, count - 1 );
+			$( selectorForm + ' .dynamic-action' ).val( 'cart/add' );
+		}
+		else {
+			current.val( count + dynamic ).submit();
+			msMiniCartDynamic.changeCartDynamic( selectorForm, count );
+			$( selectorForm + ' .dynamic-action' ).val( 'cart/change' );
+		}
+	});
+	// Обновление мини корзины без +/-
+	$(document).on('submit', 'form', function(e) {
+		var cls = $( this ).attr( 'class' );
+		if ( cls != 'dynamic-action' ) {
+			miniShop2.Callbacks.Cart.add.response.success = function( response ) {
+				if( response['success'] == true ) {
+					msMiniCartDynamic.changeDynamic( 'add' );
+				}
+			}
+		}
+		else {
+			return false;
+		}
+	});
 });
